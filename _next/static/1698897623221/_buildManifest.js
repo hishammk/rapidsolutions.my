bad2d7a917fb6e9337e5eab49b1e290ce79cee19
Muhammad Syahmi Mohd Ikram <122784412+syahmiikram@users.noreply.github.com>
2023-11-02 04:01:47 +0000

    Update navbar styling and add text in footer

---
+++ _next/static/1698897623221/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/913-c7cc273c80faa118.js","static/chunks/pages/index-fa13e09864b131f0.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
